a7ba44169befd1013005bc549fe1361735722719
Samuel M Smith <smith.samuel.m@gmail.com>
2020-10-07 10:07:16 -0600

    added function

--- src/hio/core/tcp/serving.py
+++ src/hio/core/tcp/serving.py
@@ -943,16 +943,5 @@ class ServerTls(Server):
         self.serviceCxes()
 
 
-class Peer(Server):
-    """
-    Nonblocking TCP Socket Peer Class.
-    Supports both incoming and outgoing connections.
-    """
-    def __init__(self, **kwa):
-        """
-        Initialization method for instance.
-        """
-        super(Peer, self).init(**kwa)
 
-        self.oxes = odict()  # outgoers indexed by ha
 
