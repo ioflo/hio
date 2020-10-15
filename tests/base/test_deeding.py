a7583d720ad96a86a32909a421e079666575e55c
Samuel M Smith <smith.samuel.m@gmail.com>
2020-10-15 10:55:11 -0600

    added test file for deeding.py

---
+++ tests/base/test_deeding.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+"""
+tests.core.test_deeding module
+
+"""
+import pytest
+
+from hio.base.basing import Ctl, Stt
+from hio.base import cycling, doing
+from hio.base.deeding import ServerDoer, ClientDoer
+
+def test_server_client():
+    """
+    Test ServerDoer ClientDoer classes
+    """
+
+
+    """End Test """
+
+
+
+
+if __name__ == "__main__":
+    test_server_client()
