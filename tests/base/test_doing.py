d70a9d72445563f14b971016b5915e30ed9a4f08
Samuel M Smith <smith.samuel.m@gmail.com>
2020-10-16 15:26:56 -0600

    tests for whodog generator function

--- tests/base/test_doing.py
+++ tests/base/test_doing.py
@@ -329,5 +329,146 @@ def test_whodoer_throw():
                                 State(tyme=0.25, context='exit', feed='Hi', count=4)]
 
 
+def test_whodog_break():
+    """
+    Test whodog testing function example with break to normal exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    assert ticker.tyme == 0.0
+    states = []
+
+    do = doing.whodog(states=states, ticker=ticker, tock=0.25)
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result == 1
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    result = do.send("Blue")
+    assert result == 3
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2),
+                           State(tyme=0.25, context='recur', feed='Blue', count=3)]
+
+    ticker.tick()
+    try:
+        result = do.send("Red")
+    except StopIteration as ex:
+        assert ex.value == True  #  clean return
+        assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                               State(tyme=0.0, context='recur', feed='Hello', count=1),
+                               State(tyme=0.125, context='recur', feed='Hi', count=2),
+                               State(tyme=0.25, context='recur', feed='Blue', count=3),
+                               State(tyme=0.375, context='recur', feed='Red', count=4),
+                               State(tyme=0.375, context='exit', feed='Red', count=5)]
+    """End Test """
+
+
+def test_whodog_close():
+    """
+    Test whodog testing function example with close to force exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    assert ticker.tyme == 0.0
+    states = []
+
+    do = doing.whodog(states=states, ticker=ticker, tock=0.25)
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result  == 1
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+
+
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    result = do.close()
+    assert result == None
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2),
+                           State(tyme=0.25, context='close', feed='Hi', count=3),
+                           State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+    ticker.tick()
+    try:
+        result = do.send("what?")
+    except StopIteration as ex:
+        assert ex.value == None  # not clean return
+        assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                               State(tyme=0.0, context='recur', feed='Hello', count=1),
+                               State(tyme=0.125, context='recur', feed='Hi', count=2),
+                               State(tyme=0.25, context='close', feed='Hi', count=3),
+                               State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+    """End Test """
+
+
+def test_whodog_throw():
+    """
+    Test whodog testing function example with throw to force exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    assert ticker.tyme == 0.0
+    states = []
+
+    do = doing.whodog(states=states, ticker=ticker, tock=0.25)
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result  == 1
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+
+
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    try:
+        result = do.throw(ValueError, "Bad")
+    except ValueError as ex:
+        assert ex.args[0] == "Bad"
+        assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                                State(tyme=0.0, context='recur', feed='Hello', count=1),
+                                State(tyme=0.125, context='recur', feed='Hi', count=2),
+                                State(tyme=0.25, context='abort', feed='Hi', count=3),
+                                State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+    ticker.tick()
+    try:
+        result = do.send("what?")
+    except StopIteration as ex:
+        assert ex.value == None  # not clean return
+        assert states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                                State(tyme=0.0, context='recur', feed='Hello', count=1),
+                                State(tyme=0.125, context='recur', feed='Hi', count=2),
+                                State(tyme=0.25, context='abort', feed='Hi', count=3),
+                                State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+
 if __name__ == "__main__":
-    test_dog_function()
+    test_whodog_throw()
