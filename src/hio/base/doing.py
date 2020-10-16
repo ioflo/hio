67891ece85f727b59329a479d5c367bfaef77c5e
Samuel M Smith <smith.samuel.m@gmail.com>
2020-10-16 14:39:07 -0600

    tests of new Doer

--- src/hio/base/doing.py
+++ src/hio/base/doing.py
@@ -2,19 +2,180 @@
 """
 hio.core.doing Module
 """
-from collections import deque
+from collections import deque, namedtuple
 
 from ..hioing import ValidationError, VersionError
 from . import ticking
 from ..core.tcp import serving, clienting
 
 """
-Doer.__call__ on instance returns generator
-Doer is generator creator
-
-but has extra methods and scope that plain funtion does not
-So have plain functions
+Also create bare generator functions
 
 whodo
 
 """
+
+class Doer():
+    """
+    Doer base class for hierarchical structured async coroutine like generators.
+    Doer.__call__ on instance returns generator
+    Doer is generator creator and has extra methods and attributes that plain
+    generator function does not
+
+    Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    """
+
+    def __init__(self, ticker=None, tock=0.0):
+        """
+        Initialize instance.
+        Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        """
+        self.ticker = ticker or ticking.Ticker(tyme=0.0)
+        self.tock = tock  # desired tyme interval between runs, 0.0 means asap
+
+
+    def __call__(self, **kwa):
+        """
+        Returns generator
+        Does not advance to first yield.
+        The advance to first yield effectively invodes the enter or open context
+        on the generator.
+        To enter either call .next or .send(None) on generator
+        """
+        return self.do(**kwa)
+
+
+    @property
+    def tock(self):
+        """
+        tock property getter, get ._tock
+        .tock is float desired .tyme increment in seconds
+        """
+        return self._tock
+
+
+    @tock.setter
+    def tock(self, tock):
+        """
+        desired cycle tyme interval until next run
+        0.0 means run asap,
+        set ._tock to tock
+        """
+        self._tock= abs(float(tock))
+
+
+    def do(self, ticker=None, tock=None):
+        """
+        Generator function to run this doer
+        Calling this function returns generator
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+
+        try:
+            # enter context
+
+
+            while (True):  # recur context
+                feed = (yield (self.tock))  # yields tock then waits for next send
+
+
+        except GeneratorExit:  # Exit context
+            pass
+
+        return  # return value of yield from, or yield ex.value of StopIteration
+
+State = namedtuple("State", "tyme context feed count")
+
+class WhoDoer(Doer):
+    """
+    WhoDoer supports testing with methods to record sends and yields
+
+
+    Inhereited Attributes:
+        .ticker is Ticker instance that provides relative cycle time as .ticker.tyme
+
+    Inherited Properties:
+        .tock is desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+    Inherited Methods:
+        .__call__ makes instance callable return generator
+        .do is generator function returns generator
+
+    Hidden:
+       ._tock is hidden attribute for .tock property
+
+    Attributes:
+       .states is list of State namedtuples (tyme, feed, result)
+
+    """
+
+    def __init__(self, **kwa):
+        """
+        Initialize instance.
+        Parameters:
+           ticker is Ticker instance
+           tock is float seconds initial value of .tock
+
+        """
+        super(WhoDoer, self).__init__(**kwa)
+        self.states = []
+
+
+    def do(self, ticker=None, tock=None):
+        """
+        Generator function to run this doer
+        Calling this function returns generator
+        """
+        if ticker is not None:
+            self.ticker = ticker
+        if tock is not None:
+            self.tock = tock
+        feed = "Default"
+        count = 0
+
+        try:
+            # enter context
+
+            self.states.append(State(tyme=self.ticker.tyme, context="enter", feed=feed, count=count))
+            while (True):  # recur context
+                feed = (yield (count))  # yields tock then waits for next send
+                count += 1
+                self.states.append(State(tyme=self.ticker.tyme, context="recur", feed=feed, count = count))
+                if count > 3:
+                    break  # normal exit
+
+        except GeneratorExit:  # close context close forced exit context
+            count += 1
+            self.states.append(State(tyme=self.ticker.tyme, context='close', feed=feed, count=count))
+
+        except Exception:  # abort context uncaught exception forced exit
+            count += 1
+            self.states.append(State(tyme=self.ticker.tyme, context='abort', feed=feed, count=count))
+            raise
+
+        finally: # exit context  break out normal exit
+            count += 1
+            self.states.append(State(tyme=self.ticker.tyme, context='exit', feed=feed, count=count))
+
+        return (True)  # return value of yield from, or yield ex.value of StopIteration
+
--- tests/base/test_doing.py
+++ tests/base/test_doing.py
@@ -5,19 +5,259 @@ tests.core.test_doing module
 """
 import pytest
 
-from hio.base.basing import Ctl, Stt
+import inspect
+
 from hio.base import ticking
-from hio.base import acting
+from hio.base import doing
+from hio.base.doing import State
 from hio.core.tcp import serving, clienting
 
-def test_test():
+def test_doer():
     """
-    Test
+    Test Doer base class
     """
+    tock = 1.0
+    doer = doing.Doer()
+    assert isinstance(doer.ticker, ticking.Ticker)
+    assert doer.tock == 0.0
+
+    ticker = ticking.Ticker()
+    doer = doing.Doer(ticker=ticker, tock=tock)
+    assert doer.ticker == ticker
+    assert doer.tock == tock == 1.0
+    doer.tock = 0.0
+    assert doer.tock ==  0.0
+
+    do = doer.do()
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == doer.tock == 0.0
+    result = do.send("Hello")
+    assert result == doer.tock == 0.0
+    result = do.send("Hi")
+    assert result == doer.tock == 0.0
+    result = do.close()
+    assert result == None
+
+    with pytest.raises(StopIteration):
+        result = do.send("what?")
+
+    do = doer()
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == doer.tock == 0.0
+    result = do.send("Hello")
+    assert result == doer.tock == 0.0
+    result = do.send("Hi")
+    assert result == doer.tock == 0.0
+    result = do.close()
+    assert result == None
+
+    with pytest.raises(StopIteration):
+        result = do.send("what?")
+
+    do = doer()
+    assert inspect.isgenerator(do)
+    result = next(do)
+    assert result == doer.tock == 0.0
+    result = next(do)
+    assert result == doer.tock == 0.0
+    result = next(do)
+    assert result == doer.tock == 0.0
+    result = do.close()
+    assert result == None
+
+    with pytest.raises(StopIteration):
+        result = do.send("what?")
+
+    do = doer(tock=tock)
+    assert inspect.isgenerator(do)
+    result = next(do)
+    assert result == doer.tock == tock == 1.0
+    result = next(do)
+    assert result == doer.tock == 1.0
+    result = next(do)
+    assert result == doer.tock == 1.0
+    result = do.close()
+    assert result == None
+
+    with pytest.raises(StopIteration):
+        result = do.send("what?")
+
+    doer.tock = 0.0
+
+    do = doer.do(tock=tock)
+    assert inspect.isgenerator(do)
+    result = next(do)
+    assert result == doer.tock == tock == 1.0
+    result = next(do)
+    assert result == doer.tock == 1.0
+    result = next(do)
+    assert result == doer.tock == 1.0
+    result = do.close()
+    assert result == None
+
+    with pytest.raises(StopIteration):
+        result = do.send("what?")
+
+
+
+    """End Test """
+
+def test_whodoer_break():
+    """
+    Test WhoDoer testing class with break to normal exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    doer = doing.WhoDoer(ticker=ticker, tock=0.25)
+    assert doer.ticker == ticker
+    assert doer.ticker.tock == 0.125
+    assert doer.tock == 0.25
+    assert doer.states ==  []
+    assert ticker.tyme == 0.0
+
+    do = doer.do()
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result  == 1
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+
+
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    result = do.send("Blue")
+    assert result == 3
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2),
+                           State(tyme=0.25, context='recur', feed='Blue', count=3)]
+
+    ticker.tick()
+    try:
+        result = do.send("Red")
+    except StopIteration as ex:
+        assert ex.value == True  #  clean return
+        assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                               State(tyme=0.0, context='recur', feed='Hello', count=1),
+                               State(tyme=0.125, context='recur', feed='Hi', count=2),
+                               State(tyme=0.25, context='recur', feed='Blue', count=3),
+                               State(tyme=0.375, context='recur', feed='Red', count=4),
+                               State(tyme=0.375, context='exit', feed='Red', count=5)]
+    """End Test """
+
+
+def test_whodoer_close():
+    """
+    Test WhoDoer testing class with close to force exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    doer = doing.WhoDoer(ticker=ticker, tock=0.25)
+    assert doer.ticker == ticker
+    assert doer.ticker.tock == 0.125
+    assert doer.tock == 0.25
+    assert doer.states ==  []
+    assert ticker.tyme == 0.0
+
+    do = doer.do()
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result  == 1
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+
+
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    result = do.close()
+    assert result == None
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2),
+                           State(tyme=0.25, context='close', feed='Hi', count=3),
+                           State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+    ticker.tick()
+    try:
+        result = do.send("what?")
+    except StopIteration as ex:
+        assert ex.value == None  # not clean return
+        assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                               State(tyme=0.0, context='recur', feed='Hello', count=1),
+                               State(tyme=0.125, context='recur', feed='Hi', count=2),
+                               State(tyme=0.25, context='close', feed='Hi', count=3),
+                               State(tyme=0.25, context='exit', feed='Hi', count=4)]
 
     """End Test """
 
 
+def test_whodoer_throw():
+    """
+    Test WhoDoer testing class with throw to force exit
+    """
+    ticker = ticking.Ticker(tock=0.125)
+    doer = doing.WhoDoer(ticker=ticker, tock=0.25)
+    assert doer.ticker == ticker
+    assert doer.ticker.tock == 0.125
+    assert doer.tock == 0.25
+    assert doer.states ==  []
+    assert ticker.tyme == 0.0
+
+    do = doer.do()
+    assert inspect.isgenerator(do)
+    result = do.send(None)
+    assert result == 0
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0)]
+    result = do.send("Hello")
+    assert result  == 1
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1)]
+
+
+    ticker.tick()
+    result = do.send("Hi")
+    assert result ==  2
+    assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                           State(tyme=0.0, context='recur', feed='Hello', count=1),
+                           State(tyme=0.125, context='recur', feed='Hi', count=2)]
+    ticker.tick()
+    try:
+        result = do.throw(ValueError, "Bad")
+    except ValueError as ex:
+        assert ex.args[0] == "Bad"
+        assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                                State(tyme=0.0, context='recur', feed='Hello', count=1),
+                                State(tyme=0.125, context='recur', feed='Hi', count=2),
+                                State(tyme=0.25, context='abort', feed='Hi', count=3),
+                                State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
+    ticker.tick()
+    try:
+        result = do.send("what?")
+    except StopIteration as ex:
+        assert ex.value == None  # not clean return
+        assert doer.states == [State(tyme=0.0, context='enter', feed='Default', count=0),
+                                State(tyme=0.0, context='recur', feed='Hello', count=1),
+                                State(tyme=0.125, context='recur', feed='Hi', count=2),
+                                State(tyme=0.25, context='abort', feed='Hi', count=3),
+                                State(tyme=0.25, context='exit', feed='Hi', count=4)]
+
 
 if __name__ == "__main__":
-    test_test()
+    test_whodoer_throw()
