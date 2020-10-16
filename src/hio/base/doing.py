d77656e80feb03ea36c23d63c7cfb04a74a63d17
Samuel M Smith <smith.samuel.m@gmail.com>
2020-10-16 07:27:11 -0600

    clean up

--- src/hio/base/doing.py
+++ src/hio/base/doing.py
@@ -8,3 +8,13 @@ from ..hioing import ValidationError, VersionError
 from . import ticking
 from ..core.tcp import serving, clienting
 
+"""
+Doer.__call__ on instance returns generator
+Doer is generator creator
+
+but has extra methods and scope that plain funtion does not
+So have plain functions
+
+whodo
+
+"""
