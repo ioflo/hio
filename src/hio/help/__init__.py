267f2ba22febfc5568eacb73ffe785b51f0edb37
Samuel M Smith <smith.samuel.m@gmail.com>
2020-11-04 13:30:21 -0700

    Added ogling module for logging support

--- src/hio/help/__init__.py
+++ src/hio/help/__init__.py
@@ -3,3 +3,12 @@
 hio.help package
 
 """
+
+# Setup module global oglery as package logger factory. This must be done on
+#  import to ensure global is defined so all modules in package have access to
+#  logggers via ogling.oglery.getLoggers(). May always change level and reopen log file
+#  if need be
+
+from . import ogling
+
+ogling.initOglery()  # inits once on import so ogling.oglery always defined by default
