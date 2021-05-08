cb9a96c2855aac06c98f39684fef5a706bea79e6
Samuel M Smith <smith.samuel.m@gmail.com>
2021-05-08 13:55:40 -0600

    updated extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,12 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser"]
+extensions = ['myst_parser',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.viewcode',
+              ]
 
 if os.getenv('SPELLCHECK'):
     extensions += 'sphinxcontrib.spelling',
