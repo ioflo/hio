c94875e41a9769b5131e2aa7b4b2dd28ab38db27
Samuel M Smith <sam@samuelsmith.org>
2025-02-25 19:20:46 -0700

    updated version to 0.6.17

--- src/hio/__init__.py
+++ src/hio/__init__.py
@@ -3,6 +3,6 @@
 hio package
 """
 
-__version__ = '0.6.16'  # also change in setup.py
+__version__ = '0.6.17'  # also change in setup.py
 
 from .hioing import Mixin, HioError, ValidationError, VersionError
