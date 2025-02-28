13abd07099908650c22741bb70f483447b0a003d
Samuel M Smith <sam@samuelsmith.org>
2025-02-28 13:18:57 -0700

    more doc strings

--- src/hio/base/multidoing.py
+++ src/hio/base/multidoing.py
@@ -23,17 +23,50 @@ class MultiDoer(Doer):
     """
     MultiDoer spawns multiprocesses with Doists
 
-
     See Doer for inherited attributes, properties, and methods.
 
+    Inherited Attributes:
+        done (bool): completion state:
+            True means completed
+            Otherwise incomplete. Incompletion maybe due to close or abort.
+        opts (dict): injected options into its .do generator by scheduler
+
+
+    Inherited Properties:
+        tyme (float): is float relative cycle time of associated Tymist .tyme obtained
+            via injected .tymth function wrapper closure.
+        tymth (closure): function wrapper closure returned by Tymist.tymen()
+                        method. When .tymth is called it returns associated
+                        Tymist.tyme. Provides injected dependency on Tymist
+                        tyme base.
+        tock (float): desired time in seconds between runs or until next run,
+                 non negative, zero means run asap
+
+
+    Inherited Methods:
+        __call__()  makes instance callable as generator function returning generator
+        do() generator method that returns generator
+        enter() is enter context action method
+        recur() recur context action method or generator method
+        clean() clean context action method
+        exit() exit context method
+        close() close context method
+        abort() abort context method
+        wind()  injects ._tymth dependency from associated Tymist to get its .tyme
+
+
     Attributes:
-       .count is iteration count
+       count (int): iteration count
+
+    Properties:
+
 
     """
 
     def __init__(self, load=None, **kwa):
-        """
-        Initialize instance.
+        """Initialize instance.
+
+
         """
         super(MultiDoer, self).__init__(**kwa)
         self.load = load if load is not None else dict()
