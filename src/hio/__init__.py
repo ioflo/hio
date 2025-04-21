e877fe1ae4a9a351a6c11a26b014edc234ba58e6
Samuel M Smith <sam@samuelsmith.org>
2025-04-21 17:26:52 -0600

    bump version for release of first pass at hier features

--- src/hio/__init__.py
+++ src/hio/__init__.py
@@ -3,7 +3,7 @@
 hio package
 """
 
-__version__ = '0.7.7'  # also change in setup.py
+__version__ = '0.7.8'  # also change in setup.py
 
 from .hioing import (Mixin, HioError, SizeError, ValidationError, VersionError,
                      HierError)
