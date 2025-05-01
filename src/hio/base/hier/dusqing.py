09d5dfdfa8fac182d5df342d8da1c030192f697f
Samuel M Smith <sam@samuelsmith.org>
2025-04-30 21:00:44 -0600

    update doc strings

--- src/hio/base/hier/dusqing.py
+++ src/hio/base/hier/dusqing.py
@@ -1,14 +1,14 @@
 # -*- encoding: utf-8 -*-
 """hio.base.hier.dusqing Module
 
-Provides durable double ended queue for hold hierarchical actions
+Provides durable set queue for hold hierarchical actions
 
 """
 from __future__ import annotations  # so type hints of classes get resolved later
 
 
 class Dusq():
-    """Dusq (durable ordered set FIFO queue) class when injected with
+    """Dusq (durable set queue) class when injected with
     .sdb and .key will store its ordered set durably and allow access as a FIFO
     queue
 
