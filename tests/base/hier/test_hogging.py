f7559e8e5b97bfb85e501f8d8e8f8b92cb87bc04
Samuel M Smith <sam@samuelsmith.org>
2025-09-01 19:15:49 -0600

    test Hog with vector bag

--- tests/base/hier/test_hogging.py
+++ tests/base/hier/test_hogging.py
@@ -7,13 +7,15 @@ import os
 import platform
 import tempfile
 import inspect
+from dataclasses import dataclass
+from typing import Any, Type
 
 import pytest
 
 import hio
 from hio.base import Doist, Tymist
 from hio.base.hier import Nabes, Rules, Hog, openHog, HogDoer, Hold, Bag
-from hio.help import TymeDom
+from hio.help import TymeDom, namify, registerify
 from hio.help.timing import nowIso8601  # timing so pytest mock nowIso8601 works
 
 
@@ -402,6 +404,120 @@ def test_hog_log(mockHelpingNowIso8601):
         ('_boxer_BoxerTest_tyme', '_boxer_BoxerTest_active', '_boxer_BoxerTest_tock'),
         ('tyme.value', 'active.value', 'tock.value'),
         ('0.0', 'BoxTop', '0.03125'),
+    ]
+
+    hog.close(clear=True)
+    assert not hog.opened
+    assert not hog.file
+    assert not os.path.exists(hog.path)
+
+    # Test vector hold bag with rule "every"
+    name = "cat"
+    tymist.tyme = 0.0
+    tymth = tymist.tymen()
+
+
+    @namify
+    @registerify
+    @dataclass
+    class LocationBag(TymeDom):
+        """Vector Bag dataclass
+
+        Field Attributes:
+            latN (Any):  latitude North fractional minutes
+            lonE (Any):  longitude East fractional minutes
+        """
+        latN: Any = None
+        lonE: Any = None
+
+        def __hash__(self):
+            """Define hash so can work with ordered_set
+            __hash__ is not inheritable in dataclasses so must be explicitly defined
+            in every subclass
+            """
+            return hash((self.__class__.__name__,) + self._astuple())  # almost same as __eq__
+
+
+    homeKey = hold.tokey(("home", ))
+    hold[homeKey] = LocationBag(latN=45.0, lonE=-90.0)
+
+    awayKey = hold.tokey(("away", ))
+    hold[awayKey] = LocationBag(latN=40.0, lonE=10.0)
+
+    for dom in hold.values():  # wind hold
+        if isinstance(dom, TymeDom):
+            dom._wind(tymth=tymth)
+
+    hold[tymeKey].value = tymist.tyme
+    hold[activeKey].value = boxName
+    hold[tockKey].value = tymist.tock
+    hold[homeKey].latN = 40.7607
+    hold[homeKey].lonE = -111.8939
+    hold[awayKey].latN = 39.3999
+    hold[awayKey].lonE = 8.2245
+
+    # locationKey as hit
+    hog = Hog(name=name, iops=iops, hold=hold, temp=True, rid=rid,
+              home=homeKey, away=awayKey)
+    assert hog.rule == Rules.every
+    assert not hog.started
+    assert not hog.onced
+    assert hog.hits == {'home': 'home', 'away': 'away'}
+
+    # run hog once
+    assert hog() == iops  # default returns iops
+    assert hog.hits == \
+    {
+        'tyme': '_boxer_BoxerTest_tyme',
+        'home': 'home',
+        'away': 'away'
+    }
+    assert hog.started
+    assert hog.onced
+    assert hog.first == 0.0
+    assert hog.last == 0.0
+    assert hog.rid == rid
+    assert hog.stamp == dts
+
+    hog.file.seek(0, os.SEEK_SET)  # seek to beginning of file
+    lines = hog.file.readlines()
+    lines = [tuple(line.rstrip('\n').split('\t')) for line in lines]
+    assert lines == \
+    [
+        ('rid', 'base', 'name', 'stamp', 'rule', 'count'),
+        ('BoxerTest_KQzSlod5EfC1TvKsr0VvkQ',
+         'BoxerTest',
+         'cat',
+         '2021-06-27T21:26:21.233257+00:00',
+         'every',
+         '0'),
+        ('tyme.key', 'home.key', 'away.key'),
+        ('_boxer_BoxerTest_tyme', 'home', 'away'),
+        ('tyme.value', 'home.latN', 'home.lonE', 'away.latN', 'away.lonE'),
+        ('0.0', '40.7607', '-111.8939', '39.3999', '8.2245')
+    ]
+
+    # run again
+    tymist.tick()
+    hold[tymeKey].value = tymist.tyme
+    hold[awayKey].latN = 41.5020
+    hold[awayKey].lonE = 9.5123
+
+    assert hog() == iops  # default returns iops
+    assert hog.last != hog.first  # since once does not log again
+    assert hog.last == tymist.tyme
+    hog.file.seek(0, os.SEEK_SET)  # seek to beginning of file
+    lines = hog.file.readlines()
+    lines = [tuple(line.rstrip('\n').split('\t')) for line in lines]
+    assert lines == \
+    [
+        ('rid', 'base', 'name', 'stamp', 'rule', 'count'),
+        ('BoxerTest_KQzSlod5EfC1TvKsr0VvkQ', 'BoxerTest', 'cat', '2021-06-27T21:26:21.233257+00:00', 'every', '0'),
+        ('tyme.key', 'home.key', 'away.key'),
+        ('_boxer_BoxerTest_tyme', 'home', 'away'),
+        ('tyme.value', 'home.latN', 'home.lonE', 'away.latN', 'away.lonE'),
+        ('0.0', '40.7607', '-111.8939', '39.3999', '8.2245'),
+        ('0.03125', '40.7607', '-111.8939', '41.502', '9.5123')
     ]
 
     hog.close(clear=True)
